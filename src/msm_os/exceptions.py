ad18815dfd050586ca750fcd2f10416f78f00be0
Joao Morado <joaomorado@gmail.com>
2023-11-22 01:01:44 +0000

    Add DuplicatedAppendDimValue exception

--- src/msm_os/exceptions.py
+++ src/msm_os/exceptions.py
@@ -8,3 +8,16 @@ class VariableNotFound(Exception):
         """Initialize the exception."""
         self.variable_name = variable_name
         super().__init__(f"Variable '{self.variable_name}' not found in the dataset.")
+
+
+class DuplicatedAppendDimValue(Exception):
+    """Exception raised for when a duplicated value is found in the append dim."""
+
+    def __init__(self, append_dim, append_dim_value):
+        """Initialize the exception."""
+        self.append_dim = append_dim
+        self.append_dim_value = append_dim_value
+        super().__init__(
+            f"Duplicated value '{self.append_dim_value}' found for the"
+            "append dimension '{self.append_dim}' in the dataset."
+        )
