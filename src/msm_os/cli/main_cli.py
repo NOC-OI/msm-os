59bbc31842dca182b1a0c0f01febde0c67fddc2f
Joao Morado <joaomorado@gmail.com>
2023-11-22 12:04:42 +0000

    Refactor CLI

--- src/msm_os/cli/main_cli.py
+++ src/msm_os/cli/main_cli.py
@@ -1,8 +1,7 @@
-"""msm_os command line interface."""
 import logging
 import sys
 
-from ..object_store_handler import send, update
+from ..object_store_handler import get_files, send, update
 from .argument_parser import __version__, create_parser
 
 logger = logging.getLogger(__name__)
@@ -26,33 +25,26 @@ def banner():
     )
 
 
-def msm_os():
-    """Run the msm_os cli."""
+def initialise_logging():
+    """Initialise logging configuration."""
     logging.basicConfig(
         stream=sys.stdout,
-        format="msm_os | %(levelname)s | %(asctime)s | %(message)s",
+        format="☁  msm_os ☁ | %(levelname)s | %(asctime)s | %(message)s",
         level=logging.INFO,
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    banner()
 
-    parser = create_parser()
-    args = parser.parse_args()
 
+def process_action(args):
+    """Process the selected action."""
     if len(sys.argv) == 1:
-        parser.print_help()
+        args.parser.print_help()
         sys.exit(0)
 
-    if args.variables is None:
-        variables = None
-    else:
-        variables = list(args.variables)
+    variables = list(args.variables) if args.variables is not None else None
 
     if args.action == "send":
-        if vars == "compact":
-            send_vars_indep = False
-        else:
-            send_vars_indep = True
+        send_vars_indep = args.variables == "compact"
 
         send(
             filepaths=list(args.filepaths),
@@ -60,7 +52,7 @@ def msm_os():
             store_credentials_json=args.store_credentials_json,
             variables=variables,
             append_dim=args.append_dim,
-            send_vars_indep=send_vars_indep,
+            send_vars_indep=not send_vars_indep,
             object_prefix=args.object_prefix,
             to_zarr_kwargs=None,
         )
@@ -74,8 +66,25 @@ def msm_os():
             object_prefix=args.object_prefix,
             to_zarr_kwargs=None,
         )
+
+    elif args.action == "list":
+        get_files(
+            bucket=args.bucket,
+            store_credentials_json=args.store_credentials_json,
+        )
     else:
         raise NotImplementedError(f"Action {args.action} not implemented.")
 
-    logging.info("msm_os finished successfully.")
+
+def msm_os():
+    """Run the msm_os cli."""
+    initialise_logging()
+    banner()
+
+    parser = create_parser()
+    args = parser.parse_args()
+
+    process_action(args)
+
+    logging.info("✔ msm_os terminated successfully ✔")
     sys.exit(0)
