27a621881a0d1623995f8dcf514afe46f5631bb5
Joao Morado <joaomorado@gmail.com>
2023-11-23 11:01:52 +0000

    Remove \n

--- src/msm_os/exceptions.py
+++ src/msm_os/exceptions.py
@@ -18,7 +18,7 @@ class DuplicatedAppendDimValue(Exception):
     def __init__(self, n_dupl, append_dim, first_dupl_value, last_dupl_value):
         """Initialise the exception."""
         msg_general = (
-            f"Found {n_dupl} duplicates in the append dimension '{append_dim}'.\n"
+            f"Found {n_dupl} duplicates in the append dimension '{append_dim}'."
         )
         msg_specific = f"Range: {first_dupl_value}--{last_dupl_value}."
         logging.warning(msg_general)
